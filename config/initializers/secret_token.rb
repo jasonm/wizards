fe9f8b319de410783f5554b39b2b2281f65640bf
Jason Morrison <jmorrison@thoughtbot.com>
2011-08-21 12:17:53 -0400

    initial 3.1 app generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wizards::Application.config.secret_token = '93454e91f05208d5f234c6e88610329f23b8b9e34edbe11080a0569f69435abc792e7126334ff4ec1559541cb0fdb3606fc990ce3f56c451d9645826bf872dcb'
